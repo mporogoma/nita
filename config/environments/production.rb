07ee362ecc07ed2cb89280097c97e430a7baa28e
ejoka <kamalaemmanuel86@gmail.com>
2025-05-11 22:58:51 +0300

    Edit kamal configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
    config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == “/up” } }}
+   config.ssl_options = { redirect: { exclude: ->(request) { request.path == “/up” } }}
 
 end
