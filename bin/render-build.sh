bba048bef1f53139d910e817da82cd76153676df
ejoka <emmanuelkamala@yahoo.com>
2025-05-12 09:29:34 +0300

    Edit render

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+# rails db:drop 
+# rails db:create 
+# bundle exec rails db:migrate
